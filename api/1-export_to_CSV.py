e3f06db19cef6696c0205899c9147d12f8ded756
Jean-Luc <jlucbilo@gmail.com>
2023-12-20 14:37:29 +0100

    A script that exports collected informations in .csv file format

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,33 +6,30 @@ import csv
 import requests
 from sys import argv
 
+rows = []
+""" collects todos infos in a dict """
+r_todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
+data_todos = r_todos.json()
 
-if __name__ == "__main__":
+""" collect users infos in a dict """
+r_users = requests.get('https://jsonplaceholder.typicode.com/users/')
+data_users = r_users.json()
 
-    rows = []
-    # collects todos infos in a dict
-    r_todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
-    data_todos = r_todos.json()
+""" gets the name of the employee """
+for i in data_users:
+    if i.get("id") == int(argv[1]):
+        employee = i.get("username")
 
-    # collect users infos in a dict
-    r_users = requests.get('https://jsonplaceholder.typicode.com/users/')
-    data_users = r_users.json()
+""" creates data in csv format """
+with open(argv[1] + '.csv', 'w', encoding='UTF8', newline='') as file:
+    writer = csv.writer(file, quoting=csv.QUOTE_ALL)
 
-    # gets the name of the employee
-    for i in data_users:
-        if i.get("id") == int(argv[1]):
-            employee = i.get("username")
-
-    # creates data in csv format
-    with open(argv[1] + '.csv', 'w', encoding='UTF8', newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-
-        # write results in rows
-        for i in data_todos:
-            rows = []
-            if i.get("userId") == int(argv[1]):
-                rows.append(i.get("userId"))
-                rows.append(employee)
-                rows.append(i.get("completed"))
-                rows.append(i.get("title"))
-                writer.writerow(rows)
+""" write results in rows """
+for i in data_todos:
+    row = []  # Create a new list for each TODO item
+    if i.get("userId") == int(argv[1]):
+        row.append(i.get("userId"))
+        row.append(employee)
+        row.append(i.get("completed"))
+        row.append(i.get("title"))
+        writer.writerow(row)
