381e0e61278b7170f6c20c1abc583c2d7235e4d9
Jean-Luc <jlucbilo@gmail.com>
2023-12-20 14:13:07 +0100

    The script that retrieves information from an API url

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+import json
+import requests
+import sys
+
+user_id = int(sys.argv[1])  # Convert user_id to an integer
+
+user_url = "https://jsonplaceholder.typicode.com/users"
+user_info = requests.get(f"{user_url}/{user_id}").json()
+
+EMPLOYEE_NAME = user_info.get("name")
+
+if EMPLOYEE_NAME is None:
+    print(f"Employee with ID {user_id} not found.")
+    sys.exit(1)
+
+todo_url = "https://jsonplaceholder.typicode.com/todos"
+todo_info = requests.get(todo_url).json()
+
+TASK_TITLE = []
+TOTAL_NUMBER_OF_TASKS = 0
+NUMBER_OF_DONE_TASKS = 0
+
+for task in todo_info:
+    if task.get("userId") == user_id:
+        TOTAL_NUMBER_OF_TASKS += 1
+        if task.get("completed"):
+            NUMBER_OF_DONE_TASKS += 1
+            TASK_TITLE.append(task.get("title"))
+
+print("Employee {} is done with tasks ({}/{})".format(
+    EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
